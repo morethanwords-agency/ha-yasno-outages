5a8bf2799451d503553fe309ea76f77e0bdc2ca9
MoreThanWords <services@morethanwords.agency>
2024-07-13 10:13:35 +0300

    Update api.py
    entities data issue in 2024.5.5

--- custom_components/yasno_outages/api.py
+++ custom_components/yasno_outages/api.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import recurring_ical_events
 from icalendar import Calendar
+import pytz
 
 from .const import CALENDAR_PATH
 
@@ -35,11 +36,17 @@ class YasnoOutagesApi:
                 self.calendar = recurring_ical_events.of(ical)
         return self.calendar
 
+    def convert_to_local_time(self, utc_time: datetime.datetime) -> datetime.datetime:
+        """Convert UTC time to local time."""
+        local_tz = pytz.timezone("Europe/Kiev")
+        return utc_time.replace(tzinfo=pytz.utc).astimezone(local_tz)
+
     def get_current_event(self, at: datetime.datetime) -> dict:
         """Get the current event."""
         if not self.calendar:
             return None
-        events_at = self.calendar.at(at)
+        local_time = self.convert_to_local_time(at)
+        events_at = self.calendar.at(local_time)
         if not events_at:
             return None
         return events_at[0]  # return only the first event
@@ -52,4 +59,6 @@ class YasnoOutagesApi:
         """Get all events."""
         if not self.calendar:
             return []
-        return self.calendar.between(start_date, end_date)
+        local_start_date = self.convert_to_local_time(start_date)
+        local_end_date = self.convert_to_local_time(end_date)
+        return self.calendar.between(local_start_date, local_end_date)
